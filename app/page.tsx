a22f4afc4da778cb3ef529c2ecedd9cb0cca3548
Theo Premartin <theo.premartin@etud.univ-angers.fr>
2025-02-18 13:48:32 +0100

    start quiz

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Navbar from "./components/navbar";
+import Quiz from "./pages/quiz";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <Quiz />
     </div>
   );
 }
