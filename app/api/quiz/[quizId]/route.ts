7e9ad569f73ec9a8210fd31bb47157ed0373473f
Theo Premartin <theo.premartin@etud.univ-angers.fr>
2025-02-20 09:22:29 +0100

    params to url

--- app/api/quiz/[quizId]/route.ts
+++ app/api/quiz/[quizId]/route.ts
@@ -1,9 +1,16 @@
+// app/api/quiz/[quizId]/route.ts
+
 import { NextResponse } from "next/server";
 import prisma from "../../../lib/prisma";
 import type { NextRequest } from "next/server";
 
-export async function GET(req: NextRequest, context: { params: { quizId: string } }) {
-  const { quizId } = context.params;
+export async function GET(req: NextRequest) {
+  const url = new URL(req.url); // Extraire l'URL de la requête
+  const quizId = url.pathname.split("/").pop(); // Extraire `quizId` de l'URL
+
+  if (!quizId) {
+    return NextResponse.json({ error: "Paramètre quizId manquant" }, { status: 400 });
+  }
 
   try {
     const quiz = await prisma.quiz.findUnique({
