19c13cccf850ff8cc6520af8db5fe09c310e5430
Theo Premartin <theo.premartin@etud.univ-angers.fr>
2025-02-20 13:09:12 +0100

    lqksdfjkqsfjqs

--- app/api/leaderboard/route.ts
+++
@@ -1,1 +0,0 @@
-// app/api/leaderboard/route.ts
--- app/pages/leaderboard/page.tsx
+++ app/pages/leaderboard/page.tsx
@@ -1,1 +1,11 @@
 // app/pages/leaderboard/page.tsx
+
+const Leaderboard = () => {
+  return (
+    <div className="container mx-auto px-4 py-6">
+      <h1 className="text-3xl font-bold">Hello, World!</h1>
+    </div>
+  );
+};
+
+export default Leaderboard;
