f1a1481d1b2028417e413e0d449c77ad1cd7de53
Theo Premartin <theo.premartin@etud.univ-angers.fr>
2025-02-25 10:40:33 +0100

    amelioration de l'animation de pourquoi choisir notre formation

--- app/components/PourquoiChoisirFormation.tsx
+++ app/components/PourquoiChoisirFormation.tsx
@@ -11,7 +11,7 @@ const PourquoiChoisirFormation = () => {
         className="text-6xl font-bold text-white relative"
         initial={{ opacity: 0, y: 50 }}
         whileInView={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.8, ease: "easeOut" }}
+        transition={{ duration: 1, ease: "easeInOut" }}
         viewport={{ once: true }}
       >
         Pourquoi choisir notre <span className="bg-gradient-to-r from-[#47CC88] to-[#00E5A2] text-transparent bg-clip-text">formation</span> ?
@@ -21,7 +21,7 @@ const PourquoiChoisirFormation = () => {
         className="text-lg text-[#9EA3BF] font-light max-w-2xl py-8 leading-relaxed"
         initial={{ opacity: 0, y: 50 }}
         whileInView={{ opacity: 1, y: 0 }}
-        transition={{ duration: 1, ease: "easeOut", delay: 0.2 }}
+        transition={{ duration: 1.2, ease: "easeInOut", delay: 0.2 }}
         viewport={{ once: true }}
       >
         La <strong className="text-[#47CC88]">cybersécurité</strong> peut sembler complexe, mais Ventyra la rend <strong className="text-[#47CC88]">ludique</strong> et <strong className="text-[#47CC88]">accessible</strong>. Nos formations favorisent <strong className="text-[#47CC88]">l’apprentissage en groupe</strong>, à travers des <strong className="text-[#47CC88]">mises en situation</strong> engageantes.
@@ -36,10 +36,10 @@ const PourquoiChoisirFormation = () => {
         ].map((item, index) => (
           <motion.div
             key={index}
-            className="bg-[#032720] border border-[#47CC88] px-6 py-4 rounded-xl shadow-lg transform hover:scale-105 transition-all duration-300"
-            initial={{ opacity: 0, y: 50 }}
+            className="bg-[#032720] border border-[#47CC88] px-6 py-4 rounded-xl shadow-lg"
+            initial={{ opacity: 0, y: 30 }}
             whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.8, ease: "easeOut", delay: index * 0.2 }}
+            transition={{ duration: 0.8, ease: "easeInOut", delay: index * 0.2 }}
             viewport={{ once: true }}
           >
             <h3 className="text-xl font-semibold text-white">{item.title}</h3>
