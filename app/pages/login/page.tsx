47c90c51d39702f82649a77223646037e1de1e29
Theo Premartin <theo.premartin@etud.univ-angers.fr>
2025-02-28 13:05:19 +0100

    modification route login

--- app/pages/login/page.tsx
+++ app/pages/login/page.tsx
@@ -80,7 +80,7 @@ const Login = () => {
         </form>
 
         <p className="mt-4 text-sm text-center text-[#9EA3BF]">
-          Pas encore de compte ? <Link href="/" className="text-[#47CC88]">Créer un compte</Link>
+          Pas encore de compte ? <Link href="/pages/login" className="text-[#47CC88]">Créer un compte</Link>
         </p>
       </div>
     </div>
