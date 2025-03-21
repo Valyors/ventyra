63a85eded45400466fc3b79860a6d0597ed4ef9c
Theo Premartin <theo.premartin@etud.univ-angers.fr>
2025-03-21 12:28:43 +0100

    add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@
 import type { Metadata } from "next";
 import { Sora } from "next/font/google";
 import "./globals.css";
+import Script from 'next/script'
+import { GA_TRACKING_ID } from './lib/gtag'
 
 const sora = Sora({
   variable: "--font-sora",
@@ -24,6 +26,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}');
+            `,
+          }}
+        />
+      </head>
       <body className={`${sora.variable} bg-[#032720] text-white`}>
         {children}
       </body>
