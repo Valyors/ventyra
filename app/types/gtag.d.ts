d94f5ca634da68ab71890271c2c9850374589bb6
Theo Premartin <theo.premartin@etud.univ-angers.fr>
2025-03-21 12:31:01 +0100

    fix bug

--- app/types/gtag.d.ts
+++ app/types/gtag.d.ts
@@ -1,11 +1,28 @@
+type GtagConfigParams = {
+  page_path?: string;
+  page_title?: string;
+  page_location?: string;
+  send_to?: string;
+  [key: string]: string | undefined;
+};
+
+type GtagEventParams = {
+  event_category?: string;
+  event_label?: string;
+  value?: number;
+  [key: string]: string | number | undefined;
+};
+
 declare global {
   interface Window {
     gtag: (
       command: 'config' | 'event' | 'js',
       targetId: string,
-      config?: Record<string, any>
+      params?: GtagConfigParams | GtagEventParams | Date
     ) => void;
-    dataLayer: any[];
+    dataLayer: Array<{
+      [key: string]: unknown;
+    }>;
   }
 }
 
